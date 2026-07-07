a042af784ef5f0dea0cc637d9175d3e39bc3d27f
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-08 06:01:04 +0800

    Change LineageOS branch from 23.2 to 22.2

--- s.sh
+++ s.sh
@@ -6,7 +6,7 @@ git rev-parse --is-shallow-repository && echo "Shallow clone" || echo "Full clon
  #!/bin/bash
 echo 'repo() { if [ "$1" = "init" ]; then shift; command repo init --depth=1 "$@"; else command repo "$@"; fi; }' >> ~/.bashrc
 . ~/.bashrc
-repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
 
 git -C .repo/manifests rev-parse --is-shallow-repository
 git rev-parse --is-shallow-repository && echo "Shallow clone" || echo "Full clone"
