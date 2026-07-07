08b1bd33b99aa3cd59ef84cb5a00ca3999df10ee
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-08 07:19:33 +0800

    Update and rename s.sh to a.sh

--- a.sh
+++ a.sh
@@ -5,7 +5,7 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
 cat .repo/manifests.git/config
 
 echo 'repo() { if [ "$1" = "init" ]; then shift; command repo init --depth=1 "$@"; else command repo "$@"; fi; }' >> /home/admin/.bashrc
-. /home/admin/.bashrc
+source /home/admin/.bashrc
 cat /home/admin/.bashrc
 repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
 
