69438a1f9b86a7b67d96b6b4f9fa1a4c3b8ffb0d
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-08 07:28:01 +0800

    Update and rename s.sh to a.sh

--- a.sh
+++ a.sh
@@ -6,10 +6,10 @@ cat .repo/manifests.git/config
 
 echo 'repo() { if [ "$1" = "init" ]; then shift; command repo init --depth=1 "$@"; else command repo "$@"; fi; }' >> /home/admin/.bash_aliases
 source /home/admin/.bash_aliases
-cat /home/admin/.bashrc
+cat /home/admin/.bash_aliases
 repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
 
 cat .repo/manifests.git/config
 
-repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs --depth 1
-cat .repo/manifests.git/config
+# repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs --depth 1
+# cat .repo/manifests.git/config
