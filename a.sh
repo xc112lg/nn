afecd936571b9da54ed6fc9fe695748b63063812
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-08 05:46:49 +0800

    Update and rename a.sh to s.sh

--- a.sh
+++
@@ -1,9 +0,0 @@
-ls -la .git/shallow
-
- #!/bin/bash
-echo 'repo() { if [ "$1" = "init" ]; then shift; command repo init --depth=1 "$@"; else command repo "$@"; fi; }' >> ~/.bashrc
-. ~/.bashrc
-repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
-
-ls -la .git/shallow
-
---
+++ s.sh
@@ -0,0 +1,12 @@
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
+git -C .repo/manifests.git config --get remote.origin.partialclonefilter
+git -C .repo/manifests.git config --get remote.origin.promisor
+
+ #!/bin/bash
+echo 'repo() { if [ "$1" = "init" ]; then shift; command repo init --depth=1 "$@"; else command repo "$@"; fi; }' >> ~/.bashrc
+. ~/.bashrc
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
+
+git -C .repo/manifests.git config --get remote.origin.partialclonefilter
+git -C .repo/manifests.git config --get remote.origin.promisor
+
