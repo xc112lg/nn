7efcc905bb0b11ce6f7197a37728207bed0a59d9
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-08 06:04:58 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,12 +1,11 @@
-cat .repo/manifests.git/shallow
+cat .repo/manifests.git/config
 rm -rf .repo/manifests
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
-git -C .repo/manifests rev-parse --is-shallow-repository
-git rev-parse --is-shallow-repository && echo "Shallow clone" || echo "Full clone"
- #!/bin/bash
+cat .repo/manifests.git/config
+
 echo 'repo() { if [ "$1" = "init" ]; then shift; command repo init --depth=1 "$@"; else command repo "$@"; fi; }' >> ~/.bashrc
 . ~/.bashrc
+
 repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
 
-git -C .repo/manifests rev-parse --is-shallow-repository
-git rev-parse --is-shallow-repository && echo "Shallow clone" || echo "Full clone"
+cat .repo/manifests.git/config
