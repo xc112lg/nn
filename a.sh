7cecdfa15722c84bb05231c079e48589cbc0622b
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-08 05:51:15 +0800

    Add shallow clone check to a.sh
    Added a check to determine if the repository is a shallow clone.

--- a.sh
+++ a.sh
@@ -1,10 +1,10 @@
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
 git -C .repo/manifests rev-parse --is-shallow-repository
-
+git rev-parse --is-shallow-repository && echo "Shallow clone" || echo "Full clone"
  #!/bin/bash
 echo 'repo() { if [ "$1" = "init" ]; then shift; command repo init --depth=1 "$@"; else command repo "$@"; fi; }' >> ~/.bashrc
 . ~/.bashrc
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
 
 git -C .repo/manifests rev-parse --is-shallow-repository
-
+git rev-parse --is-shallow-repository && echo "Shallow clone" || echo "Full clone"
