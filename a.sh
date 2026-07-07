f90908f2a45dc9af96eafd84c0e83bad7cbebba4
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-08 05:32:20 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,3 +1,4 @@
+grep -R "clone-depth" .repo/manifests .repo/manifest.xml 2>/dev/null
  #!/bin/bash
 echo 'repo() { if [ "$1" = "init" ]; then shift; command repo init --depth=1 "$@"; else command repo "$@"; fi; }' >> ~/.bashrc
 . ~/.bashrc
