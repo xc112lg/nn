f9d402b6dd6d2e54570d463d497f54e97e559270
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-08 05:42:44 +0800

    Update and rename s.sh to a.sh

--- a.sh
+++ a.sh
@@ -1,7 +1,9 @@
-grep -R "clone-depth" .repo/manifests .repo/manifest.xml 2>/dev/null
+ls -la .git/shallow
+
  #!/bin/bash
 echo 'repo() { if [ "$1" = "init" ]; then shift; command repo init --depth=1 "$@"; else command repo "$@"; fi; }' >> ~/.bashrc
 . ~/.bashrc
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
 
-grep -R "clone-depth" .repo/manifests .repo/manifest.xml 2>/dev/null
+ls -la .git/shallow
+
